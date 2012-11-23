799c42d003528badef44cc0f452a3d1637a4a612
duynguyen <nguyentuongduy1988@gmail.com>
2012-11-23 05:52:05 -0800

    fix block 2

--- edit_node_71.php
+++ edit_node_71.php
@@ -4,3 +4,20 @@
 	$node = node_load(71);
 	$node->body['und'][0]['value'] = $content;
 	node_save($node);
+
+	
+	global $base_url;
+	// update block
+	$block_content = "<div style='padding-top: 10px;'>";
+	$block_content .= "<img src='{$base_url}" ."/sites/default/files/ontheair.png' alt='On the air' style='float: right;' />";
+	$block_content .= "<a href='http://www.hcam.tv/live-stream' style='float: right;'>";
+	$block_content .= "<img src='{$base_url}" ."/sites/default/files/online.png' alt='On-line' />";
+	$block_content .= "</a></div>";
+	$block_content .= "<div style='clear: both;'><!-- --></div>";
+	
+	db_update('block_custom') // Table name no longer needs {}
+	  ->fields(array(
+		'body' => $block_content,
+	  ))
+	  ->condition('bid', 2)
+	  ->execute();
